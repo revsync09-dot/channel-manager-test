f352b70147fc05368516b8f3800178e65c6115cf
Red_thz <248220094+revsync09-dot@users.noreply.github.com>
2025-12-10 22:49:45 +0100

    Delete Channel-bot - Kopie directory

--- Channel-bot - Kopie/test_record.py
+++
@@ -1,7 +0,0 @@
-from src.database import Database
-from datetime import datetime
-import os
-os.chdir('C:\\Users\\subha\\Downloads\\Channel-bot')
-db=Database()
-db.record_user_activity(1234,5678,chat_minutes=1,activity_date='2025-12-09')
-print('inserted')
--- Channel-bot - Kopie/view_db.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn=sqlite3.connect('bot_data.db')
-cursor=conn.cursor()
-cursor.execute('SELECT guild_id,user_id,activity_date,chat_minutes,voice_minutes FROM user_activity')
-print(cursor.fetchall())
-conn.close()
